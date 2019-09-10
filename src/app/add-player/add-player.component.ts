704de726f7f232dde67681d9677b1652faa5af54
Arjan <mundyarjan@gmail.com>
2019-09-10 00:31:14 -0500

    add player component refactored
    Added form validations and improved flow on posting User

--- src/app/add-player/add-player.component.ts
+++ src/app/add-player/add-player.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { PlayerService } from '../services/player.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AppError } from '../common/apperror';
+import { map, switchMap, catchError} from 'rxjs/operators';
+import { Observable, combineLatest, throwError, } from 'rxjs';
 
 @Component({
   selector: 'app-add-player',
@@ -13,9 +17,24 @@ export class AddPlayerComponent implements OnInit {
   phoneNumber: string;
   email: string;
   memberOf: number;
-  constructor(private service: PlayerService) { }
+  parameter: number;
+  constructor(
+    private service: PlayerService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).pipe(
+      switchMap( combined => {
+        let param = combined[0].getAll('id')[0];
+        return param;
+      })).subscribe( id => {
+        this.parameter = +id;
+        console.log(this.parameter);
+      });
   }
 
   addPlayer() {
@@ -28,8 +47,9 @@ export class AddPlayerComponent implements OnInit {
 
     }).subscribe((response) => {
       console.log(response);
-      alert(response.firstName +' player added');
-      //this.players.push(response);
+      this.router.navigate(['/golfCourses/'+this.parameter+'/players']);
+      //alert(response.firstName +' player added');
+
     }, error => {
       alert('An unexpected error occured.');
     });
